f0047b1acc3b5e866ef1545f0290a4bb1869acc6
Aravind Srinivasan <aravindsrinivasan96@gmail.com>
2021-11-04 11:29:56 -0700

    Enable mouse mode and highlight on yank

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -1,11 +1,13 @@
 -- Global
 local o = vim.o
 o.autoread = true
-o.smartcase = true
 o.ignorecase = true
+o.smartcase = true
 o.termguicolors = true
 o.syntax = 'on'
 o.confirm = true
+o.mouse = 'a'
+o.breakindent = true
 
 -- Window
 local wo = vim.wo
@@ -23,3 +25,13 @@ bo.autoindent = true
 bo.smartindent = true
 bo.syntax = 'on'
 
+-- Highlight on yank
+vim.api.nvim_exec(
+  [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]],
+  false
+)
