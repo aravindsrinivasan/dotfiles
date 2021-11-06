dd4e2fecf43565e3b1b02049f339173bca622f55
Aravind Srinivasan <aravindsrinivasan96@gmail.com>
2021-11-05 18:59:12 -0700

    Convert setting to vim.opt with some quality of life imporvements

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -1,29 +1,28 @@
--- Global
-local o = vim.o
-o.autoread = true
-o.ignorecase = true
-o.smartcase = true
-o.termguicolors = true
-o.syntax = 'on'
-o.confirm = true
-o.mouse = 'a'
-o.breakindent = true
-
--- Window
-local wo = vim.wo
-wo.number = true
-wo.relativenumber = true
-wo.signcolumn = 'yes'
-
--- Buffer
-local bo = vim.bo
-bo.tabstop = 2
-bo.expandtab = true
-bo.shiftwidth = 2
-bo.softtabstop = 2
-bo.autoindent = true
-bo.smartindent = true
-bo.syntax = 'on'
+vim.opt.autoread = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.syntax = 'on'
+vim.opt.confirm = true
+vim.opt.mouse = 'a'
+vim.opt.breakindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.syntax = 'on'
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window.
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window.
+vim.opt.updatetime = 300 -- faster completion.
+vim.opt.cursorline = true -- highlight the current line.
+vim.opt.signcolumn = "yes" -- always show the sign column otherwise it would shift the text each time.
+vim.opt.scrolloff = 8 -- Lines to keep above and below cursor.
+vim.opt.sidescrolloff = 8
 
 -- Highlight on yank
 vim.api.nvim_exec(
