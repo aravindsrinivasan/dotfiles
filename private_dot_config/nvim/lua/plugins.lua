743ad2a2cc931288c5f2cb9f460e87dfd0e2ec2b
Aravind Srinivasan <aravindsrinivasan96@gmail.com>
2021-11-15 11:47:48 -0800

    Configure lualine

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -78,14 +78,26 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources={'nvim_lsp', 'coc'}}},
-    lualine_c = {},
+                  {'diagnostics', sources={'nvim_lsp'}}},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,  -- displays file status (readonly status, modified status)
+        path = 1,            -- 0 = just filename, 1 = relative path, 2 = absolute path
+        shorting_target = 40 -- Shortens path to leave 40 space in the window
+      }
+    },
     lualine_x = {'filetype'},
     lualine_y = {'location'},
     lualine_z = {'progress'}
   },
   tabline = {
-    lualine_a = {'buffers'},
+    lualine_a = {
+      {
+        'buffers',
+        show_filename_only = true, -- shows shortened relative path when false
+      }
+    },
     lualine_b = {},
     lualine_c = {},
     lualine_x = {},
