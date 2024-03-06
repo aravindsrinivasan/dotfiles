28abaf21ca432b63afe42bdfae1cd264f3f9e9f9
Aravind <aravind@harvey.ai>
2024-03-05 16:47:44 -0800

    Disable scope

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -87,7 +87,11 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     config = function()
-      require('ibl').setup()
+      require('ibl').setup({
+        scope = {
+          enabled = false,
+        }
+      })
     end,
   },
 
